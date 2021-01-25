1e2aa12a42d1e7859c5953405b56cd3081b5786f
lindellcarternyc <lindellcarternyc@gmail.com>
2021-01-25 17:31:36 -0500

    complete task 2

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(data) {
+   return data.filter(match => match.Stage === 'Final')
 }
 
 
